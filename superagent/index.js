07486cd56af3ead82701545ee328dcfbf1607a08
zhai <zhai@mogu61.com>
2018-01-04 19:27:21 +0800

    finished

--- superagent/index.js
+++ superagent/index.js
@@ -1,11 +1,13 @@
 const superagent = require('superagent')
 const cheerio = require('cheerio')
+const fs = require('fs')
 
 function spiderFun (url, cb) {
   return new Promise((resolve, reject) => {
     superagent.get(url).end((err, res) => {
       if (err) {
-        console.log('err', err)
+        console.log('url', url)
+        resolve(null)
         return
       }
       let $ = cheerio.load(res.text)
@@ -17,59 +19,52 @@ function spiderFun (url, cb) {
 async function getArticle (url) {
   // var url = 'http://www.ruanyifeng.com/blog/2017/12/travis_ci_tutorial.html'
   var $ = await spiderFun(url)
+  if (!$) {
+    return null
+  }
   var res = {
     title: $('.page-title').text(),
-    content: [
-      ...Array.from($('#main-content').children()).map(elm => {
-        var node = $(elm)
-        if (elm.tagName === 'h2') {
+    content: Array.from($('#main-content').children()).map(elm => {
+      var node = $(elm)
+      if (elm.tagName === 'h2') {
+        return {
+          type: 'h2',
+          text: node.text()
+        }
+      } else {
+        if (node.find('img').length) {
+          var img = $(node.find('img'))
           return {
-            type: 'h2',
-            text: node.text()
+            type: 'img',
+            src: img.attr('src')
           }
         } else {
-          if (node.find('img').length) {
-            var img = $(node.find('img'))
-            return {
-              type: 'img',
-              src: img.attr('src')
-            }
-          } else {
-            return {
-              type: 'p',
-              text: node.text()
-            }
+          return {
+            type: 'p',
+            text: node.text()
           }
         }
-      }),
-      {
-        type: 'h2', // p, h2
-        text: '编写代码只是软件开发的一小部分，更多的时间往往花在构建（build）和测试（test）。' // pre
-      },
-      {
-        type: 'img',
-        src: 'http://www.ruanyifeng.com/blogimg/asset/2017/bg2017121901.png'
       }
-    ]
+    })
   }
-  console.log('getArticle', res)
+  // console.log('getArticle', res)
   return res
 }
 
 async function getSubClass ({url, name}) {
   var $ = await spiderFun(url)
-  var subGroups = Array.from($('#content .module-content'))
+  var subGroups = $($('#content #alpha .module-content')[0])
   var groupName = ''
-  var articles = subGroups.children().map(grElm => {
+  var pages = Array.from(subGroups.children()).map(grElm => {
     var gr = $(grElm)
-    if (grElm.tagName === 'H3') {
+    if (grElm.tagName === 'h3') {
       groupName = gr.text()
-    } else if (grElm.tagName === 'UL') {
+    } else if (grElm.tagName === 'ul') {
       var links = Array.from(gr.find('a'))
       var articleList = links.map(linkElm => {
         var link = $(linkElm)
         return {
-          url: link.attr('src'),
+          url: link.attr('href'),
           title: link.text()
         }
       })
@@ -79,41 +74,38 @@ async function getSubClass ({url, name}) {
       }
     }
   })
-  articles = articles.filter(Boolean)
-  console.log('getSubClass', articles)
-  return new Promise((resolve, reject) => {
-    var res = []
-    articles.forEach(async (page, idx) => {
-      var article = await getArticle(page.url)
-      res.push(article)
-      if (idx === articles.length - 1) {
-        resolve(res)
-      }
-    })
-  })
+  pages = pages.filter(Boolean)
+  // console.log('getSubClass', pages)
+  return pages
+  // return new Promise((resolve, reject) => {
+  //   pages.forEach(async (page, idx) => {
+  //     page.articleList.forEach(async (article, j) => {
+  //       Object.assign(article, await getArticle(article.url))
+  //       // console.log('\n\n\narticle\n\n\n')
+  //       if (idx === pages.length - 1 && j === page.articleList.length - 1) {
+  //         resolve(pages)
+  //       }
+  //     })
+  //   })
+  // })
 }
 
 async function getCls () {
   var url = 'http://www.ruanyifeng.com/blog/opinions/'
   var $ = await spiderFun(url)
   var lis = Array.from($('#beta-inner .module-list-item a'))
-  var res = [
-    ...lis.map(li => {
-      li = $(li)
-      return {
-        url: li.attr('src'),
-        name: li.text()
-      }
-    }),
-    {
-      url: 'http://www.ruanyifeng.com/blog/essays/',
-      name: '散文'
+  var res = lis.map(li => {
+    li = $(li)
+    return {
+      url: li.attr('href'),
+      name: li.text()
     }
-  ]
-  console.log('getCls', res)
+  })
+  // console.log('getCls', res)
   return new Promise((resolve, reject) => {
-    res.forEach((group, idx) => {
-      group.children = getSubClass(group)
+    res.forEach(async (group, idx) => {
+      group.children = await getSubClass(group)
+      // console.log('\n\n\ngroup.children\n\n\n')
       if (idx === res.length - 1) {
         resolve(res)
       }
@@ -121,5 +113,12 @@ async function getCls () {
   })
 }
 
-var res = JSON.stringify(getCls())
-console.log('===============\n\n\n\n', res, '\n\n\n\n===============')
+getCls().then(res => {
+  var res = JSON.stringify(res)
+  fs.appendFile('tmp.txt', res, (err) => {
+    if (err) {
+      console.log('err', err)
+    }
+  })
+  // console.log('===============\n\n\n\n', res, '\n\n\n\n===============')
+})
